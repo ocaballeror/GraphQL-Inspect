b5f80cf4cc1f4a48b0b477185f94fe153bdbe8c1
Oscar Caballero <ocaballeror@tutanota.com>
2026-07-17 12:57:57 +0200

    remove plugin visualizer and stats html

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(async (_env) => {
-    const { visualizer } = await import('rollup-plugin-visualizer')
-    return {
-        build: {
-            outDir: 'dist/firefox',
-            sourcemap: 'inline' as const,
-            emptyOutDir: false,
-            minify: false,
-        },
-        plugins: [
-            react(),
-            visualizer({
-              template: 'network'
-            })
-        ]
-    }
+export default defineConfig({
+    build: {
+        outDir: 'dist/firefox',
+        sourcemap: 'inline' as const,
+        emptyOutDir: false,
+        minify: false,
+    },
+    plugins: [
+        react(),
+    ]
 })
