be7c1dc66d9506e7aa44742a40d85b6242697ab0
Oscar Caballero <ocaballeror@tutanota.com>
2026-09-16 11:11:27 +0200

    sign extension from build script

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 npm run build:firefox
 
 echo "Built extension to dist/firefox"
+
+cd dist/firefox
+npx web-ext sign --api-key="$MOZILLA_JWT_ISSUER" --api-secret="$MOZILLA_JWT_SECRET" --channel=unlisted
+cd ../..
